0fe59f527a89b6e013f8c2e07121739f241c666d
Mustafa Alshaar <mustafa5555885555@gmail.com>
2026-02-01 11:09:50 +0300

    use taskStatus constants in Task schema

--- models/task.js
+++ models/task.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
-
+const taskStatus = require("../utils/taskStatus");
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: false },
-    status : { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
+    status : { type: String, enum: [taskStatus.PENDING, taskStatus.IN_PROGRESS, taskStatus.COMPLETED], default: taskStatus.PENDING },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 
 },
